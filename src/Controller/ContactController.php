a24c9d55f801e121464a0955dbc939e86c8e4d50
D <lorrainedams@me.com>
2019-12-09 07:41:07 +0100

    suite

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+
 use App\Entity\Contact;
 use App\Form\ContactType;
 use App\Repository\ContactRepository;
@@ -9,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Swift_Mailer;
 
 /**
  * @Route("/")
@@ -28,7 +30,7 @@ class ContactController extends AbstractController
     /**
      * @Route("/", name="contact_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, \Swift_Mailer $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
@@ -38,57 +40,25 @@ class ContactController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($contact);
             $entityManager->flush();
+            $message = (new \Swift_Message('Bienvenue chez WikiCampers'))
+                ->setFrom('mercibien8@gmail.com')
+                ->setTo('mercibien8@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'email/contactemail.html.twig'
+
+                    ),
+                    'text/html'
+                );
+            $mailer->send($message);
 
             return $this->redirectToRoute('contact_index');
+            }
+
+            return $this->render('contact/new.html.twig', [
+                'contact' => $contact,
+                'form' => $form->createView(),
+            ]);
+
         }
-
-        return $this->render('contact/new.html.twig', [
-            'contact' => $contact,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="contact_show", methods={"GET"})
-     */
-    public function show(Contact $contact): Response
-    {
-        return $this->render('contact/show.html.twig', [
-            'contact' => $contact,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="contact_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Contact $contact): Response
-    {
-        $form = $this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('contact_index');
-        }
-
-        return $this->render('contact/edit.html.twig', [
-            'contact' => $contact,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="contact_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Contact $contact): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$contact->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($contact);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('contact_index');
-    }
 }
